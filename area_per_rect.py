3df3d0d9c87fb3ac24ac2312b0de1f1d2606f97c
Alexander Mehertab <alexander.mehertab@stu.ocsb.ca>
2023-09-22 01:59:17 +0000

    area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by:Alex
+# Created on: sep. 21, 2023
+# This program calculates and displays the area and perimeter of a
+# rectangle
+
+
+def main():
+    print("For a rectangle that has dimensions")
+    print("length 5cm and width 7.4:")
+    print("Perimeter = {} cm".format(2 * (5 + 7.4)))
+    print("")
+    print("For a rectangle that has dimensions")
+    print("length 5cm and width 7:")
+
+    print("Area = {} cm^2".format(5 * 7))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Alex
+# Created on: sep. 21, 2023
+# This program does basic math.
+
+
+def main():
+    print("9 + 3 = {}".format(9 + 3))
+    print("77 - 3 = {}".format(77 - 3))
+    print("44 / 2 = {}".format(44 / 2))
+    print("8 - 1 / 3 = {}".format(8 - 1 / 3))
+    print("2^5 = {}".format(2**5))
+    print("1 / 9 = {}".format(1 / 9))
+
+
+if __name__ == "__main__":
+    main()
